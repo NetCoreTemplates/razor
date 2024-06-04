826fdd0de09e4a78e92e213b0ac0802044d02e64
Demis Bellot <demis.bellot@gmail.com>
2024-06-04 16:43:41 +0800

    Add support for API Keys

--- MyApp/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ MyApp/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -59,6 +59,12 @@ namespace  MyApp.Areas.Identity.Pages.Account.Manage
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
+        public static string ApiKeys => "ApiKeys";
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
         public static string TwoFactorAuthentication => "TwoFactorAuthentication";
 
         /// <summary>
@@ -108,6 +114,12 @@ namespace  MyApp.Areas.Identity.Pages.Account.Manage
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
         public static string TwoFactorAuthenticationNavClass(ViewContext viewContext) => PageNavClass(viewContext, TwoFactorAuthentication);
+
+        /// <summary>
+        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+        ///     directly from your code. This API may change or be removed in future releases.
+        /// </summary>
+        public static string ApiKeysNavClass(ViewContext viewContext) => PageNavClass(viewContext, ApiKeys);
 
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
